8b370c41e6efff6da1c7510486cf793a9b622663
padmapriya114 <46240280+padmapriya114@users.noreply.github.com>
2021-06-01 11:31:51 +0530

    Update queries.js

--- queries.js
+++ queries.js
@@ -8,13 +8,7 @@ const pool = new Pool({
 });
 
 const getopportunity = (request, response) => {
-  pool.query('SELECT A.opportunity_status_id,A.leadFee_servicefee,A.currentbalanceinpennies,A.updated,A.percentcommitted,A.contractid,A.loan_id,A.percentfunded,A.id,A.lead_name,A.lead_userid,A.lead_fi_id,A.grading,A.location_state,A.rate,A.dueby,A.term,A.created,A.retaining,A.resourcetype,A.description FROM	cbanc2.opportunity A
-INNER JOIN cbanc2.fi B
-       ON A.lead_fi_id = B.id
-INNER JOIN cbanc2.loan C
-       ON A.loan_id = C.id
-INNER JOIN cbanc2.loan_resource_type D
-       ON C.loan_resource_type_id = D.id', (error, results) => {
+  pool.query('SELECT A.opportunity_status_id,A.leadFee_servicefee,A.currentbalanceinpennies,A.updated,A.percentcommitted,A.contractid,A.loan_id,A.percentfunded,A.id,A.lead_name,A.lead_userid,A.lead_fi_id,A.grading,A.location_state,A.rate,A.dueby,A.term,A.created,A.retaining,A.resourcetype,A.description FROM	cbanc2.opportunity AINNER JOIN cbanc2.fi B ON A.lead_fi_id = B.id INNER JOIN cbanc2.loan C ON A.loan_id = C.id INNER JOIN cbanc2.loan_resource_type D ON C.loan_resource_type_id = D.id', (error, results) => {
     if (error) {
       throw error
     }
